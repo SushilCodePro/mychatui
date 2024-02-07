63536f87dda42b0a7436767f0fe7b923144ebdc3
Sushil Gupta <sushil.codepro@gmail.com>
2024-02-07 14:47:57 +0530

    attach and smile icon added

--- src/Chat.js
+++ src/Chat.js
@@ -10,11 +10,24 @@ function Chat() {
       <div className='chat-header'>{name}</div>
       <p>Hi All</p>
       <div className='send-input'>
+        <div className='attach-icon'>
+          <img src='https://cdn-icons-png.flaticon.com/128/8138/8138518.png' alt='attach-icon' />
+        </div>
         <input
           type='text'
+          placeholder='Type your message here'
           value={''}
           onChange={''}
         />
+        <div className='attach-icon'>
+          <img src='https://cdn-icons-png.flaticon.com/128/742/742751.png' alt='attach-icon' />
+        </div>
+        <div className='attach-icon'>
+          <img src='https://cdn-icons-png.flaticon.com/128/9484/9484251.png' alt='attach-icon' />
+        </div>
+        <div className='drop-icon'>
+          <img src='https://cdn-icons-png.flaticon.com/128/3426/3426514.png' alt='drop icon' />
+        </div>
         <div className='send-icon'>
           <img src='https://cdn-icons-png.flaticon.com/128/3682/3682321.png' alt='send icon' />
         </div>
